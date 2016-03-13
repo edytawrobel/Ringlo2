cae495da71ffccf31c7b84b0e87ebbb28495f80e
Edyta Wrobel <wrobeledyta.ew@gmail.com>
2016-03-13 23:30:53 +0000

    Third commit - bulding up

--- app.py
+++ app.py
@@ -8,7 +8,53 @@ def landingPage():
     return render_template("index.html")
 
 
-@app.route("/send", methods=['POST'])
+@app.route("/signup", methods=['POST'])
+def signup():
+    return render_template('signup.html')
+
+    global _name
+    global _email
+    global _city
+    global _season
+    global _giphy
+
+    # read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _city = request.form['inputCity']
+    _animal = request.form['inputSeason']
+
+
+    # Giphy https://github.com/Giphy/GiphyAPI
+    data = json.loads(urllib.urlopen("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5").read())
+    _giphy = "http://api.giphy.com/v1/gifs/random?fmt=html&tag={}&api_key=dc6zaTOxFJmzC".format(_season)
+
+    # validate the received values
+    if _name and _email and _city and _season:
+        print "All good!"
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+
+    _sendTo = request.form['inputEmail']
+
+    requests.post(
+        "https://api.mailgun.net/v3/sandbox4b9b1d94381b48b4b05732cffa0da0ac.mailgun.org/messages",
+        auth=("api", "key-6c19c1c364273bc85bb70777ef854618"),
+        data={"from": "Ringlo Team <postmaster@sandbox4b9b1d94381b48b4b05732cffa0da0ac.mailgun.org>",
+              "to": "User <"+_sendTo+">",
+              "subject": "Newsletter Signup",
+              "html": "<iframe src="+_giphy+" width='150px' height='150px' allowFullScreen></iframe>" +
+                      "This is a message from "+_name+" from "+_city+", " +
+                      "where it looks like: "+_season
+
+    })
+
+@app.route("/thankYou")
+def thankYou():
+    return render_template("thankYou.html") #Confirmation page
+
+@app.route("/messagesent", methods=['POST'])
 def send():
 
 # Your Account Sid and Auth Token from twilio.com/user/account
@@ -24,6 +70,8 @@ def send():
     smss = client.sms.messages.list()
     import pdb; pdb.set_trace()
 
+    return render_template('messagesent.html', _name=_name)
+
     #with open('my_csv.csv', 'w') as f:
         #writer = csv.Writer()
         #for sms in smss:
@@ -33,26 +81,5 @@ def send():
     #https://api.twilio.com/2010-04-01/Accounts/AC651dcd0b578ee9f523abfb0de332b948/SMS/Messages.csv?PageSize=1000
     # export the raw data of messages from twilio
 
-@app.route('/signup.html', methods=['POST'])
-def signup():
-
-    global _name
-    global _email
-    global _city
-
-    _sendTo = request.form['inputEmail']
-
-    requests.post(
-        "https://api.mailgun.net/v3/sandbox4b9b1d94381b48b4b05732cffa0da0ac.mailgun.org/messages",
-        auth=("api", "key-6c19c1c364273bc85bb70777ef854618"),
-        data={"from": "Mailgun Sandbox <postmaster@sandbox4b9b1d94381b48b4b05732cffa0da0ac.mailgun.org>",
-              "to": "User <"+_sendTo+">",
-              "subject": "User details",
-              "html": "<iframe src="++" width='150px' height='150px' allowFullScreen></iframe>" +
-                      "This is a message from "+_name+" from "+_city+", "
-
-    }
-    )
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
