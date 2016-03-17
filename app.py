aeda267b37b72bb2b1ecdb5c903e6ab216c4fbef
Harini Manivannan <harini.manivannan@gmail.com>
2016-03-17 11:05:45 +0000

    exported twilio data into csv

--- app.py
+++ app.py
@@ -62,24 +62,27 @@ def send():
     auth_token  = "c65b08717f3dd6727a4c845809088cb8"
     client = TwilioRestClient(account_sid, auth_token)
 
-    message = client.messages.create(body="Jenny please?! I love you <3",
+    message = client.messages.create(body="I'm currently walking down a dim lit area. Please stay in touch with me.",
         to="+447853263417",    # Replace with your phone number
         from_="+441278393077") # Replace with your Twilio number
     print message.sid
 
+    # get a list of all the sms messages from twilio
     smss = client.sms.messages.list()
-    import pdb; pdb.set_trace()
 
-    return render_template('messagesent.html', _name=_name)
+    # writes all the sms messages into a csv file
+    with open('my_smslist.csv', 'w') as f:
+        writer = csv.writer(f)
+        for sms in smss:
+            writer.writerow([sms.date_sent, sms.body, sms.from_, sms.to])
+            print sms.date_sent, sms.body, sms.from_, sms.to
 
-    #with open('my_csv.csv', 'w') as f:
-        #writer = csv.Writer()
-        #for sms in smss:
-            #writer.write(sms.to, sms.from_, sms.message)
-
+    #return render_template('messagesent.html', _name=_name)
 
     #https://api.twilio.com/2010-04-01/Accounts/AC651dcd0b578ee9f523abfb0de332b948/SMS/Messages.csv?PageSize=1000
     # export the raw data of messages from twilio
+
+    # import pdb; pdb.set_trace()
 
 if __name__ == "__main__":
     app.run(debug=True)
